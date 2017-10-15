f71dbb28efc38efe7834ae6256d7261d144b05a5
Ola <defto94@gmail.com>
2017-10-15 14:40:47 +0200

    Log4j in AgeController class

--- src/main/java/hello/AgeController.java
+++ src/main/java/hello/AgeController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AgeController {
+    final static Logger logger = Logger.getLogger(AgeController.class);
 
     @RequestMapping("/age")
     public String greeting(@RequestParam(value="age", required=false, defaultValue="How old are you?") String age, Model model) {
         model.addAttribute("age", age);
+        logger.info("User has : " + age);
         return "age";
     }
 
